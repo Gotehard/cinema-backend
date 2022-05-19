bdcfcbaccdf9069ed8fd0a68f3b6a606851de58f
Maciej <gotehard666@gmail.com>
2022-05-20 00:47:18 +0200

    dev #4 Add refresh token in auth controller

--- src/main/java/pl/macstr/cinema/controller/AuthController.java
+++ src/main/java/pl/macstr/cinema/controller/AuthController.java
@@ -1,17 +1,22 @@
 package pl.macstr.cinema.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.macstr.cinema.dao.UserRepository;
 import pl.macstr.cinema.model.User;
+import pl.macstr.cinema.service.TokenService;
 
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    @Value("${jwt.token.key}")
+    String key;
     UserRepository repository;
 
     public AuthController(UserRepository repository) {
@@ -19,19 +24,27 @@ public class AuthController {
     }
 
     @PostMapping("/sign-up")
-    User RegisterUser(@RequestBody User user){
+    User RegisterUser(@RequestBody User user) {
         user.setActive(true);
         user.setRoles("ROLE_USER");
         Optional<User> inDBUser = repository.findByUserName(user.getUserName());
 
-        if (inDBUser.isPresent()){
+        if (inDBUser.isPresent()) {
             return null;
         }
         inDBUser = repository.findByEmail(user.getEmail());
-        if (inDBUser.isPresent()){
+        if (inDBUser.isPresent()) {
             return null;
         }
         //todo move into service
         return repository.save(user);
+    }
+
+    @PostMapping(value = "/refresh", produces = "application/json")
+    String refreshToken(@RequestBody String refToken) {
+        String userName = TokenService.getUserNameFromToken(refToken, key);
+        String token = TokenService.createToken(userName, key);
+        String rToken = TokenService.createRenewToken(userName, key);
+        return "{\"token\":\"" + token + "\",\"refreshToken\":\"" + rToken + "\"}";
     }
 }
