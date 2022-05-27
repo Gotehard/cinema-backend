ff875f7f632de646df90823ffde8f725f25828ad
Nawojka <k.sopinska@gmail.com>
2022-05-27 20:24:51 +0200

    test

--- src/test/java/pl/macstr/cinema/CinemaApplicationTests.java
+++ src/test/java/pl/macstr/cinema/CinemaApplicationTests.java
@@ -1,13 +1,22 @@
 package pl.macstr.cinema;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+import pl.macstr.cinema.model.Movie;
+import pl.macstr.cinema.service.MovieService;
 
 @SpringBootTest
 class CinemaApplicationTests {
 
+    @Autowired
+    MovieService movieService;
+
     @Test
     void contextLoads() {
+        Iterable<Movie> movies =  movieService.findAll();
+        Assert.notNull(movies);
     }
 
 }
